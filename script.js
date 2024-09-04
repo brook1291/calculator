5fb88749f799f8baede1dbef7efbdebba1ec1409
DOP <167078399+brook1291@users.noreply.github.com>
2024-09-04 08:16:20 +0100

    Add operate function

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const divide = (a, b) => a / b;
 let firstNum;
 let operator;
 let secondNum;
+
+function operate(num1, operator, num2) {
+  if (operator === '+') add(num1, num2);
+  if (operator === '-') subtract(num1, num2);
+  if (operator === '*') multiply(num1, num2);
+  if (operator === '/') divide(num1, num2);
+}
