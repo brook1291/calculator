26d0a95f81c8921e2d92aa87e21b5a8d02ac89ae
DOP <167078399+brook1291@users.noreply.github.com>
2024-09-05 19:33:42 +0100

    Calculate the number after another operation is clicked

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const add = (a, b) => a + b;
-const subtract = (a, b) => a + b;
+const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
@@ -38,6 +38,27 @@ const multiplyBtn = document.querySelector('#multiply');
 const divideBtn = document.querySelector('#divide');
 const operations = [addBtn, subtractBtn, multiplyBtn, divideBtn];
 
+operations.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    if (operator === '/' && secondNum === 0) {
+      alert("Don't do that!");
+      return;
+    }
+    if (firstNum && operator) {
+      display.textContent = operate(firstNum, operator, secondNum);
+      displayValue = display.textContent;
+    }
+
+    if (e.target.id === 'add') operator = '+';
+    if (e.target.id === 'subtract') operator = '-';
+    if (e.target.id === 'multiply') operator = '*';
+    if (e.target.id === 'divide') operator = '/';
+
+    firstNum = Number(displayValue);
+    secondNum = false;
+  });
+});
+
 numInputArr.forEach((node) => {
   node.addEventListener('click', populateDisplay);
 });
@@ -49,13 +70,16 @@ decimalPoint.addEventListener('click', (e) => {
 });
 
 function operate(num1, operator, num2) {
-  if (operator === '+') add(num1, num2);
-  if (operator === '-') subtract(num1, num2);
-  if (operator === '*') multiply(num1, num2);
-  if (operator === '/') divide(num1, num2);
+  if (operator === '+') return add(num1, num2);
+  if (operator === '-') return subtract(num1, num2);
+  if (operator === '*') return multiply(num1, num2);
+  if (operator === '/') return divide(num1, num2);
 }
 
 function populateDisplay(e) {
+  if (secondNum === false) {
+    display.textContent = '0';
+  }
   if (display.textContent === '0' && e.target.id !== 'decimal') {
     display.textContent = e.target.textContent;
   } else if (display.textContent === '0' && e.target.id === 'decimal') {
@@ -64,4 +88,5 @@ function populateDisplay(e) {
     display.textContent += e.target.textContent;
   }
   displayValue = display.textContent;
+  secondNum = Number(displayValue);
 }
